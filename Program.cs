86b82f7abbf842b658d3e17f1986f1fe2cf30e83
Nayab Zahra <rarezahra1214@gmail.com>
2026-05-16 19:20:29 +0500

    Fixed Jenkins File

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using MyDevOpsProject.Data;
+using Prometheus;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -32,6 +33,8 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseHttpMetrics(); 
+app.MapMetrics();
 
 app.UseAuthorization();
 
